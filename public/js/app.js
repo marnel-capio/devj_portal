a75d7604b5dd5bcbe47d734691cbaa3b7e6d0e8d
Hazel Nera <hazel.nera@awsys-i.com>
2022-10-25 08:16:40 +0800

    commented out code for idle timer

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,27 @@
 
-//idle timer
-var idleTime  = 0;
+// //idle timer
+// var idleTime  = 0;
 
-$(document).ready(function(){
-    setInterval(idleTimer, 000);   //increment timer every 1 minute
+// $(document).ready(function(){
+//     setInterval(idleTimer, 000);   //increment timer every 1 minute
 
-    $(this).mousemove(resetIdleTime);
-    $(this).mouseleave(resetIdleTime);
-    $(this).keypress(resetIdleTime);
-    $(this).load(resetIdleTime);
-    $(this).click(resetIdleTime);
-    $(window).unload(resetIdleTime);
+//     $(this).mousemove(resetIdleTime);
+//     $(this).mouseleave(resetIdleTime);
+//     $(this).keypress(resetIdleTime);
+//     $(this).load(resetIdleTime);
+//     $(this).click(resetIdleTime);
+//     $(window).unload(resetIdleTime);
 
-});
+// });
 
-function resetIdleTime(){
-    idleTime = 0;
-}
+// function resetIdleTime(){
+//     idleTime = 0;
+// }
 
-function idleTimer(){
-    idleTime++;
-    if(idleTime > 10){
-        location.replace('/devj_portal/public/logout'); //need to replace
-    }
-}
+// function idleTimer(){
+//     idleTime++;
+//     if(idleTime > 10){
+//         location.replace('/devj_portal/public/logout'); //need to replace
+//     }
+// }
 
