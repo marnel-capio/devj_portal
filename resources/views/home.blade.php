7ca8894e01dd7ad22f18d9314d1a2d327823d6e6
justLsrn <justine.laserna@awsys-i.com>
2023-09-14 18:06:30 +0800

    Proposed changes for Homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,75 +1,114 @@
 @include('header')
 
 @include('headerMenu')
-<div class="container container-req-table  mt-4 ms-4 mb-4">
-  @if(auth()->user()->roles == 2)
 
-	<div class="row-req-table row group-category-home p-2">
-	    <div class="col table-avoid-overflow">
-	      <h3 class="mb-4"> Employee Request </h3>
-			<table id="employee-request" class="table table-striped request-table" >
-		        <thead>
-		            <tr>
-		                <th>Name</th>
-		                <th>Email Address</th>
-		                <th>Position</th>
-		                <th>Actions</th>
-		            </tr>
-		        </thead>
-		        <tbody>
-		        	@foreach ($employee_request as $user)
-		            <tr>
-		                <td>{{$user['last_name']}}, {{$user['first_name']}}</td>
-		                <td>{{$user['email']}}</td>
-		                <td>
-		                @if ($user['position'] == 1) 
-		                	{{config('constants.POSITION_1_NAME')}}
-		                @elseif ($user['position'] == 2) 
-		                	{{config('constants.POSITION_2_NAME')}}
-		                @elseif ($user['position'] == 3) 
-		                	{{config('constants.POSITION_3_NAME')}}
-		                @elseif ($user['position'] == 4) 
-		                	{{config('constants.POSITION_4_NAME')}}
-		                @elseif ($user['position'] == 5) 
-		                	{{config('constants.POSITION_5_NAME')}}
-		                @elseif ($user['position'] == 6) 
-		                	{{config('constants.POSITION_6_NAME')}}
-		                @elseif ($user['position'] == 7) 
-		                	{{config('constants.POSITION_7_NAME')}}
-		                @elseif ($user['position'] == 8) 
-		                	{{config('constants.POSITION_8_NAME')}}
-		                @elseif ($user['position'] == 9) 
-		                	{{config('constants.POSITION_9_NAME')}}
-		                @else
-		                	-
-		                @endif
-						</td>
-		                <td>	
-		                	<a href="{{ url("/employees/{$user['id']}/request") }}" alt="View"><i class="bi bi-eye"></i>View</a>
-		                </td>
-		            </tr>
-		            @endforeach
-		        </tbody>
-		    </table>
-	    </div>
+<div class="container container-req-table">
+	<div class="row-req-table row">
+		<div class="px-0">
+			<div>
+				<h3 class="mb-4"> Welcome, {{ auth()->user()->first_name }}</h3>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col col-lg-7 py-2 px-4">
+					</div>
+					<div class="group-category-home border rounded border-light col col-lg-5 py-2 px-4">
+						<h4 class="mt-1">Requests</h4>
+						<div class="dash-summary-row row mb-1">
+							<div class="col col-md-11 col-sm-9"><a href="#div-employee-request"><span class="dash-summary dash-header">Employee Request</span></a> </div><div class="dash-summary dash-count col col-md-1 col-sm-1">{{$employee_request->count()}}</div>
+						</div>
+						<div class="dash-summary-row row mb-1">
+							<div class="col col-md-11 col-sm-9"><a href="#div-laptop-request"><span class="dash-summary dash-header">Laptop Request</span></a> </div><div class="dash-summary dash-count col col-md-1 col-sm-1">{{$laptopRequest != null ? count($laptopRequest) : "0"}}</div>
+						</div>
+						<div class="dash-summary-row row mb-1">
+							<div class="col col-md-11 col-sm-9"><a href="#div-laptop-link-request"><span class="dash-summary dash-header">Laptop Link</span></a> </div><div class="dash-summary dash-count col col-md-1 col-sm-1">{{$laptopLinkRequest != null ? count($laptopLinkRequest) : "0"}}</div>
+						</div>
+						<div class="dash-summary-row row mb-1">
+							<div class="col col-md-11 col-sm-9"><a href="#div-software-request"><span class="dash-summary dash-header">Software Request</span></a> </div><div class="dash-summary dash-count col col-md-1 col-sm-1">{{$softwareRequest != null ? $softwareRequest->count() : "0"}}</div>
+						</div>
+						<div class="dash-summary-row row mb-1">
+							<div class="col col-md-11 col-sm-9"><a href="#div-project-link-request"><span class="dash-summary dash-header">Project Link</span></a> </div><div class="dash-summary dash-count col col-md-1 col-sm-1">{{$projectLinkRequest != null ? count($projectLinkRequest) : "0"}}</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
-	@endif
-	<div class="row-req-table row group-category-home p-2"> 
+
+	@if(auth()->user()->roles == 2)
+	<div id="div-employee-request" class="row-req-table row group-category-home p-2">
 		<div class="col table-avoid-overflow">
-		  <h3 class="mb-4"> Laptop Request </h3>
-			<table id="laptop-request" class="table table-striped request-table" >
+		<h3 class="mb-4"> Employee Request </h3>
+			@if(count($employee_request) > 0)
+			<table id="employee-request" class="table table-striped request-table" >
 				<thead>
 					<tr>
-						<th style="width:18%">Tag Number</th>
-						<th style="width:18%">PEZA Form Number</th>
-						<th style="width:18%">PEZA Permit Number</th>
-						<th style="width:12%">Make</th>
-						<th style="width:12%">Model</th>
-						<th style="width:11%">Status</th>
-						<th style="width:11%">Action</th>
+						<th>Name</th>
+						<th>Email Address</th>
+						<th>Position</th>
+						<th>Actions</th>
 					</tr>
 				</thead>
 				<tbody>
+				@foreach ($employee_request as $user)
+					<tr>
+						<td>{{$user['last_name']}}, {{$user['first_name']}}</td>
+						<td>{{$user['email']}}</td>
+						<td>
+						@if ($user['position'] == 1) 
+							{{config('constants.POSITION_1_NAME')}}
+						@elseif ($user['position'] == 2) 
+							{{config('constants.POSITION_2_NAME')}}
+						@elseif ($user['position'] == 3) 
+							{{config('constants.POSITION_3_NAME')}}
+						@elseif ($user['position'] == 4) 
+							{{config('constants.POSITION_4_NAME')}}
+						@elseif ($user['position'] == 5) 
+							{{config('constants.POSITION_5_NAME')}}
+						@elseif ($user['position'] == 6) 
+							{{config('constants.POSITION_6_NAME')}}
+						@elseif ($user['position'] == 7) 
+							{{config('constants.POSITION_7_NAME')}}
+						@elseif ($user['position'] == 8) 
+							{{config('constants.POSITION_8_NAME')}}
+						@elseif ($user['position'] == 9) 
+							{{config('constants.POSITION_9_NAME')}}
+						@else
+							-
+						@endif
+						</td>
+						<td>	
+							<a href="{{ url("/employees/{$user['id']}/request") }}" alt="View"><i class="bi bi-eye"></i>View</a>
+						</td>
+					</tr>
+				@endforeach
+				</tbody>
+			</table>
+			@else
+				<div>
+					<p class="text-center">No data available in table</p>
+				</div>
+			@endif
+		</div>
+	</div>
+	@endif
+	<div id="div-laptop-request" class="row-req-table row group-category-home p-2"> 
+		<div class="col table-avoid-overflow">
+		<h3 class="mb-4"> Laptop Request </h3>
+			@if(count($laptopRequest) > 0)
+			<table id="laptop-request" class="table table-striped request-table" >
+					<thead>
+						<tr>
+							<th style="width:18%">Tag Number</th>
+							<th style="width:18%">PEZA Form Number</th>
+							<th style="width:18%">PEZA Permit Number</th>
+							<th style="width:12%">Make</th>
+							<th style="width:12%">Model</th>
+							<th style="width:11%">Status</th>
+							<th style="width:11%">Action</th>
+						</tr>
+					</thead>
+					<tbody>
 					@foreach ($laptopRequest as $request)
 						<tr>
 							<td>{{ $request['tag_number'] }}</a></td>
@@ -81,13 +120,20 @@
 							<td><a href="{{ route('laptops.request', ['id' => $request['id']]) }}"><i class="bi bi-eye"></i>View</a></td>
 						</tr>
 					@endforeach
-				</tbody>
+					</tbody>
 			</table>
+			@else
+				<div class="col">
+					<p class="text-center">No data available in table</p>
+				</div>
+			@endif
 		</div>
 	</div>
-	<div class="row-req-table row group-category-home p-2">
+	{{-- This section is for Laptop Link list --}}
+	<div id="div-laptop-link-request" class="row-req-table row group-category-home p-2">
 		<div class="col table-avoid-overflow">
-		  <h3 class="mb-4"> Laptop Link Request </h3>
+		<h3 class="mb-4"> Laptop Link Request </h3>
+			@if(count($laptopLinkRequest) > 0)
 			<table id="laptop-link-request" class="table table-striped request-table" >
 				<thead>
 					<tr>
@@ -110,12 +156,18 @@
 					@endforeach
 				</tbody>
 			</table>
+			@else
+				<div>
+					<p class="text-center">No data available in table</p>
+				</div>
+			@endif
 		</div>
 	</div>
 	{{-- This section is for Software list --}}
-	<div class="row-req-table row group-category-home p-2"> 
+	<div id="div-software-request" class="row-req-table row group-category-home p-2"> 
 		<div class="col table-avoid-overflow">
 			<h3 class="mb-4"> Software Request </h3>
+			@if(count($softwareRequest) > 0)
 			<table id="software-request" class="table table-striped request-table" >
 				<thead>
 					<tr>
@@ -136,12 +188,18 @@
 					@endforeach
 				</tbody>
 			</table>
+			@else
+				<div>
+					<p class="text-center">No data available in table</p>
+				</div>
+			@endif
 		</div>
 	</div>	
 	{{-- This section is for Project Link list --}}
-	<div class="row-req-table row group-category-home p-2"> 
+	<div id="div-project-link-request" class="row-req-table row group-category-home p-2"> 
 		<div class="col table-avoid-overflow">
 			<h3 class="mb-4"> Project Link Request </h3>
+			@if(count($projectLinkRequest) > 0)
 			<table id="project-link-request" class="table table-striped request-table" >
 				<thead>
 					<tr>
@@ -168,6 +226,11 @@
 					@endforeach
 				</tbody>
 			</table>
+			@else
+				<div>
+					<p class="text-center">No data available in table</p>
+				</div>
+			@endif
 		</div>
 	</div>
 </div>
