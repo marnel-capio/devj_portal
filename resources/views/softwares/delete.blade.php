e4ae0c2b92593337e25705654deb66e1fa12741d
Justine Laserna <justine.laserna@awsys-i.com>
2023-10-05 22:58:08 +0800

    Fix: • grammar on laptops completion page. • upload of missing file

---
+++ resources/views/softwares/delete.blade.php
@@ -0,0 +1,10 @@
+@include("header")
+
+
+@if (Auth::check())
+    <div class="vh-100 d-flex flex-column justify-content-center align-items-center">
+        <div><h4 class="text-secondary mb-4">Software has been deleted successfully!</h4></div>
+        <a href="{{ route('home') }}" class="text-center">Home</a></p>
+    </div>
+@endif
+@include('footer')
